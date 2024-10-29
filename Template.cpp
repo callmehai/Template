02e625f17b499739c3251e48dc0369f5459144b9
callmehai <108572472+callmehai@users.noreply.github.com>
2024-10-29 17:39:26 +0700

    Update Template.cpp

--- Template.cpp
+++ Template.cpp
@@ -1,44 +1,45 @@
+
 #pragma GCC optimize ("O3")
 
 //#include <bits/stdc++.h>
 
-#include <iostream>     // Input/output stream objects
-#include <fstream>      // File stream objects
-#include <sstream>      // String stream objects
-#include <iomanip>      // Input/output manipulators
-#include <string>       // String class and functions
-#include <vector>       // Dynamic array
-#include <list>         // Doubly linked list
-#include <set>          // Set container
-#include <map>          // Map container
-#include <queue>        // Queue container
-#include <stack>        // Stack container
-#include <algorithm>    // Algorithms on sequences (e.g., sort, find)
-#include <cmath>        // Mathematical functions
-#include <ctime>        // Date and time functions
-#include <cstdlib>      // General purpose functions (e.g., memory management)
-#include <cstring>      // C-style string functions
-#include <cctype>       // Character classification functions
-#include <cassert>      // Assert function for debugging
-#include <exception>    // Standard exceptions
-#include <functional>   // Function objects
-#include <iterator>     // Iterator classes
-#include <limits>       // Numeric limits
-#include <locale>       // Localization and internationalization
-#include <numeric>      // Numeric operations (e.g., accumulate)
-#include <random>       // Random number generators
-#include <stdexcept>    // Standard exception classes
-#include <typeinfo>     // Runtime type information
-#include <utility>      // Utility components (e.g., std::pair)
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <iomanip>
+#include <string>
+#include <vector>
+#include <list>
+#include <set>
+#include <map>
+#include <queue>
+#include <stack>
+#include <algorithm>
+#include <cmath>
+#include <ctime>
+#include <cstdlib>
+#include <cstring>
+#include <cctype>
+#include <cassert>
+#include <exception>
+#include <functional>
+#include <iterator>
+#include <limits>
+#include <locale>
+#include <numeric>
+#include <random>
+#include <stdexcept>
+#include <typeinfo>
+#include <utility>
 #include <tuple>
 #include <cstdio>
 #include <bitset>
 
 using namespace std;
 
-// ************ Setting up  ************
+// ************ Define ************
 #define FPTU ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
-#define TIME  (1.0 * clock() / CLOCKS_PER_SEC)
+#define TIME (1.0 * clock() / CLOCKS_PER_SEC)
 #define BUG(x) {cerr << #x << " = " << x << endl;}
 #define pii pair<int,int>
 #define pip pair<int,pii>
@@ -60,6 +61,7 @@ using namespace std;
 #define all(arr) arr.begin(),arr.end()
 #define bit(n, i) (((n) >> (i)) & 1)
 #define db(x) cerr << #x << " = " << (x) << '\n';
+#define el cout<<'\n';
 
 ll rand_num(ll a,ll b) {
     random_device rd;
@@ -70,17 +72,17 @@ ll rand_num(ll a,ll b) {
 
 int mod=1e9+7; // MODDDDDDDDDDDDD
 
-template <typename T> void minimize(T &a, T b){ if(a>b)  a=b; }
-template <typename T> void maximize(T &a, T b){ if(a<b)  a=b; }
-template <typename T> void add(T &a, T b){ a+=b; if(a>=mod) a-=mod; }
-template <typename T> T gcd(T a, T b)
+template<typename T>void minimize(T &a, T b){ if(a>b)  a=b; }
+template<typename T>void maximize(T &a, T b){ if(a<b)  a=b; }
+template<typename T>void add(T &a, T b){ a+=b; if(a>=mod) a-=mod; }
+template<typename T> T gcd(T a, T b)
 {
-    while(a!=0&&b!=0)
-    {
-        if(a>b) a%=b;
-        else b%=a;
+    if(a<b) swap(a,b);
+    while(a){
+        b%=a;
+        swap(a,b);
     }
-    return a+b;
+    return b;
 }
 vector<pair<int,int>> queen  = {{-1,0},{0,-1},{0,1},{1,0},{-1,-1},{-1,1},{1,-1},{1,1}};
 vector<pair<int,int>> knight = {{-1,-2},{-1,2},{1,-2},{1,2},{-2,-1},{-2,1},{2,-1},{2,1}};
@@ -92,6 +94,7 @@ void read_file()
     freopen("sample.inp","r",stdin);
     freopen("sample.out","w",stdout);
 }
+
 // =========> <3 VietHai1709 <3  <=========
 
 void Minnnnnnn()
@@ -99,17 +102,22 @@ void Minnnnnnn()
 
 }
 int main(){
-    FPTU; read_file();
+    FPTU;
+//    read_file();
+    
     int Test_case=1;
-    //cin>>Test_case;
-    while(Test_case--)
+//    cin>>Test_case;
+    
+    for(int _=1;_<=Test_case;_++)
     {
+//        cout<<"Case #"<<_<<": ";
         Minnnnnnn();
+        el;
     }
+    
     cerr << "Time elapsed: " << TIME << " s.\n";
     return 0;
 }
-
 
 /*
 ******************************
@@ -128,7 +136,7 @@ int main(){
 *
 *     (\_/)
 *     (^_^)
-*     />V<\
+*     />o<\
 *
 *******************************************************************
 *                                                                 *
